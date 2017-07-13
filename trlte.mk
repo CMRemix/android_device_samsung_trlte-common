4d20aa1c10d443f4d3452b2dc52681a060e1b00b
ZION959 <minhdtran43@gmail.com>
2017-07-12 21:01:34 -0700

    trlte-common: Move to commonized, overlay-based GPS config & remove old gps hal
    kang from klte-common
    
    * This is one main step toward future consolidating of some klte*
      builds
    * Approach taken from Nexus/Pixel devices, values based on
      previously functional variant gps.conf files combined with
      LPP_PROFILE tuning by carrier from marlin
    
    Change-Id: Ibdadb10e92766678d76fb06b82ca7a9447c2067c

--- trlte.mk
+++ trlte.mk
@@ -151,6 +151,7 @@ PRODUCT_PACKAGES += \
 
 # GPS config
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/clatd.conf:system/etc/clatd.conf \
     $(LOCAL_PATH)/configs/flp.conf:/system/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
     $(LOCAL_PATH)/configs/izat.conf:/system/etc/izat.conf \
